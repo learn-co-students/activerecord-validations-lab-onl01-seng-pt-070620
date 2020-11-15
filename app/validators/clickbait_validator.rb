d3d8e7f452df9d4c8d6aa92bd6ecbfe794981a8f
gracenak <gracenak@gmail.com>
2020-11-15 09:46:40 -0700

    Done.

--- app/validators/clickbait_validator.rb
+++ app/validators/clickbait_validator.rb
@@ -1,5 +1,7 @@
 class ClickbaitValidator < ActiveModel::Validator
     def validate(record)
-
+        unless record.title.include?("Won't Believe" || "Secret" || "Top [number]" ||"Guess")
+            record.errors[:title] << "Not clickbait"
+        end
     end
 end
