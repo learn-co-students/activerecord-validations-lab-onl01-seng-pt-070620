b0672eb4d0420b91bcf49aa69ba02e84e95beb7f
eulis01 <eulis01@gmail.com>
2020-11-19 16:57:39 -0500

    add post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+    validates_with PostValidator
+    validates :title, presence: true, uniqueness: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-fiction"] }
 end
